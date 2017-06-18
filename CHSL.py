886358adc4eb581ebd41cda59c1437e4df2a4f5c
Ofri <ofrik89@gmail.com>
2017-06-18 22:51:34 +0300

    continued to implement.. added the score function for the CV

--- CHSL.py
+++ CHSL.py
@@ -6,14 +6,21 @@ import random
 from sklearn.svm import SVC
 from sklearn.cluster import KMeans
 import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
+from sklearn.metrics import classification_report
+from sklearn.metrics import recall_score
+from sklearn.metrics import confusion_matrix
 
 
 class CHSL(object):
-    def __init__(self, linearClassifier, clusteringAlgorithm, numberOfClusters):
+    def __init__(self, linearClassifier, clusteringAlgorithm, numberOfClusters, paramGrid):
         self.models = []
         self.baseLinearClassifier = clone(linearClassifier)
         self.numberOfClusters = numberOfClusters
         self.baseClusteringAlgorithm = clone(clusteringAlgorithm.set_params(**{"n_clusters": numberOfClusters}))
+        self.paramGrid = paramGrid
+        self.scoreFunc = make_scorer(self.score_func)
 
     def fit(self, X, y):
         self.models = []
@@ -33,6 +40,7 @@ class CHSL(object):
         """
         clustersLabels = self.baseClusteringAlgorithm.fit(mjr_X).labels_
         for i in range(1, self.numberOfClusters + 1):
+            # TODO set the class of minority to be 1 the other to be 0
             tmp_clf = clone(self.baseLinearClassifier)
             subMjrClusterIndexes = np.where(clustersLabels == (i - 1))
             subMjrCluster_X = mjr_X.iloc[subMjrClusterIndexes]
@@ -82,6 +90,43 @@ class CHSL(object):
             preds.append(test_res)
         return preds
 
+    def score_func(self, y, y_pred, **kwargs):
+        """
+        score for the CV, positive is labeling 1 (the minority class)
+        :param y: true labels
+        :param y_pred: predicted labels
+        :param kwargs:
+        :return:
+        """
+        b = kwargs.get("b")
+        matrix = confusion_matrix(y, y_pred)
+        TN = matrix[0][0]
+        TP = matrix[1][1]
+        FP = matrix[0][1]
+        FN = matrix[1][0]
+        sensitivity = float(TP) / (TP + FN)
+        specificity = float(TN) / (TN + FP)
+        if specificity > b:
+            return sensitivity
+        else:
+            return 0
+
+
+class CHSLOptimizer(CHSL):
+    def __init__(self, models):
+        self.models = models
+
+    def fit(self, X, y):
+        for model in self.models:
+            model.fit(X, y)
+
+    def predict(self, X):
+        return super(CHSLOptimizer, self).predict(X)
+
+    def set_params(self, **params):
+        for model in self.models:
+            model.set_params(params)
+
 
 if __name__ == "__main__":
     numberOfMajor = 1000
@@ -92,7 +137,7 @@ if __name__ == "__main__":
     mnr2 = [random.uniform(101, 150) for i in range(numberOfMinor)]
     df = pd.DataFrame(
         {"value1": mjr1 + mnr1, "value2": mjr2 + mnr2, "target": ([0] * numberOfMajor) + ([1] * numberOfMinor)})
-    clf = CHSL(SVC(), KMeans(), 2)
+    clf = CHSL(SVC(kernel="linear"), KMeans(), 2)
     y = df["target"]
     X = df.drop("target", axis=1)
     clf.fit(X, y)
